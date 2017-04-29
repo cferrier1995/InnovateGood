0fa8dba03b12a002fce6f734e8bcfb4307cf6a6d
Dayyan <dayyan1@umbc.edu>
2017-04-29 18:41:47 -0400

    added status to each request

--- imports/ui/requests.js
+++ imports/ui/requests.js
@@ -11,5 +11,16 @@ Template.requests.helpers({
   },
   format(date){
     return date.ToDateString();
+  },
+  statusName(){
+  	console.log("You seeing these rq's")
+  	if(this.status == 0)
+  		return "Requested"
+  	if(this.status == 1)
+  		return "Pending"
+  	if(this.status == 2)
+  		return "Deliverd"
+  	if(this.status == 3)
+  		return "Cancled"
   }
 });
